e746973f3755f09d6fb8739bbfb8627d7a5ee000
Anthony Wang <hugh1234@mail.ustc.edu.cn>
2015-11-17 11:42:23 +0800

    trackpad for libinput

--- bin/trackpad-toggle.sh
+++ bin/trackpad-toggle.sh
@@ -1,2 +1,3 @@
 #!/bin/bash - 
-synclient TouchpadOff=$(synclient -l | grep -c 'TouchpadOff.*=.*0')
+#synclient TouchpadOff=$(synclient -l | grep -c 'TouchpadOff.*=.*0')
+xinput set-prop 14 138 $(xinput list-props  14|awk '/138/{print !$4} ')
