d7c4676968c33368b1b156abcdf7a6f517229810
Anthony Wang <hugh1234@mail.ustc.edu.cn>
2015-11-17 17:42:33 +0800

    add lua prompt hack, change editor

--- rc.lua
+++ rc.lua
@@ -1,3 +1,4 @@
+require("repetitive")
 package.path = package.path .. ";./?/init.lua;"
 
 local gears = require("gears")
@@ -196,6 +197,101 @@ do
     --]]
 
 end
+-- hack for lua eval
+function lua_completion (line, cur_pos, ncomp)
+   -- Only complete at the end of the line, for now
+   if cur_pos ~= #line + 1 then
+      return line, cur_pos
+   end
+
+   -- We're really interested in the part following the last (, [, comma or space
+   local lastsep = #line - (line:reverse():find('[[(, ]') or #line)
+   local lastidentifier
+   if lastsep ~= 0 then
+      lastidentifier = line:sub(lastsep + 2)
+   else
+      lastidentifier = line
+   end
+
+   local environment = _G
+
+   -- String up to last dot is our current environment
+   local lastdot = #lastidentifier - (lastidentifier:reverse():find('.', 1, true) or #lastidentifier)
+   if lastdot ~= 0 then
+      -- We have an environment; for each component in it, descend into it
+      for env in lastidentifier:sub(1, lastdot):gmatch('([^.]+)') do
+         if not environment[env] then
+            -- Oops, no such subenvironment, bail out
+            return line, cur_pos
+         end
+         environment = environment[env]
+      end
+   end
+
+   local tocomplete = lastidentifier:sub(lastdot + 1)
+   if tocomplete:sub(1, 1) == '.' then
+      tocomplete = tocomplete:sub(2)
+   end
+   local completions = {}
+   for k, v in pairs(environment) do
+      if type(k) == "string" and k:sub(1, #tocomplete) == tocomplete then
+         table.insert(completions, k)
+      end
+   end
+
+   if #completions == 0 then
+      return line, cur_pos
+   end
+   while ncomp > #completions do
+      ncomp = ncomp - #completions
+   end
+
+   local str = ""
+   if lastdot + lastsep ~= 0 then
+      str = line:sub(1, lastsep + lastdot + 1)
+   end
+   str = str .. completions[ncomp]
+   cur_pos = #str + 1
+   return str, cur_pos
+end
+
+function usefuleval(s)
+	local f, err = loadstring("return "..s);
+	if not f then
+		f, err = loadstring(s);
+	end
+	if f then
+		setfenv(f, _G);
+		local ret = { pcall(f) };
+		if ret[1] then
+			-- Ok
+			table.remove(ret, 1)
+			local highest_index = #ret;
+			for k, v in pairs(ret) do
+				if type(k) == "number" and k > highest_index then
+					highest_index = k;
+				end
+				ret[k] = select(2, pcall(tostring, ret[k])) or "<no value>";
+			end
+			-- Fill in the gaps
+			for i = 1, highest_index do
+				if not ret[i] then
+					ret[i] = "nil"
+				end
+			end
+			if highest_index > 0 then
+				naughty.notify({ awful.util.escape("Result"..(highest_index > 1 and "s" or "")..": "..tostring(table.concat(ret, ", "))), screen = mouse.screen});
+			else
+				naughty.notify({ "Result: Nothing", screen = mouse.screen})
+			end
+		else
+			err = ret[2];
+		end
+	end
+	if err then
+		naughty.notify({ awful.util.escape("Error: "..tostring(err)), screen = mouse.screen})
+	end
+end
 
 -- }}}
 
@@ -261,12 +357,12 @@ local tools = {
 tools.browser.primary = "google-chrome-stable"
 tools.browser.secondary = "firefox"
 
-tools.editor.primary = os.getenv("EDITOR") or "gvim"
-tools.editor.secondary = ({emacs="gvim", gvim="emacs"})[tools.editor.primary]
+--tools.editor.primary = os.getenv("EDITOR") or "gvim"
+--tools.editor.secondary = ({emacs="gvim", gvim="emacs"})[tools.editor.primary]
 
 -- alternative: override
---tools.editor.primary = "gvim"
---tools.editor.secondary = "emacs"
+tools.editor.primary = "gvim"
+tools.editor.secondary = "emacs"
 
 local myapp = nil
 do
@@ -1331,6 +1427,7 @@ for s = 1, screen.count() do
     layout:set_right(right_layout)
 
     mywibox[s]:set_widget(layout)
+    mywibox[s].visible=false
 end
 
 util.taglist.set_taglist(mytaglist)
@@ -1534,7 +1631,7 @@ awful.key({modkey}, "F4", function()
     awful.prompt.run(
     {prompt = "Run Lua code: "},
     mypromptbox[mouse.screen].widget,
-    awful.util.eval, nil,
+    awful.util.eval, lua_completion,
     awful.util.getdir("cache") .. "/history_eval"
     )
 end),
