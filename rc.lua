4245eb44d57a08304bb62178c430ee156a30fca6
Wei "pw" Peng <write.to.peng.wei@gmail.com>
2016-01-01 16:22:15 -0800

    1.7.14: Bind Sakura to terminal key, but also install terminator for its Quake-style invocation.

--- rc.lua
+++ rc.lua
@@ -55,7 +55,7 @@ customization.option = {}
 customization.timer = {}
 customization.widgets = {}
 
-customization.config.version = "1.7.13"
+customization.config.version = "1.7.14"
 customization.config.help_url = "https://github.com/pw4ever/awesome-wm-config/tree/" .. customization.config.version
 
 customization.default.property = {
@@ -262,7 +262,7 @@ end
 
 --{{
 local tools = {
-    terminal = "terminator",
+    terminal = "sakura",
     system = {
         filemanager = "pcmanfm",
     },
