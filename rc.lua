ebcdf6dc87eb846fa07855696ca20dd55da9d0ae
Wei "pw" Peng <write.to.peng.wei@gmail.com>
2018-09-08 16:08:51 -0700

    4.0.13-dev: Start new development cycle.

--- rc.lua
+++ rc.lua
@@ -63,7 +63,7 @@ customization.option = {}
 customization.timer = {}
 customization.widgets = {}
 
-customization.config.version = "4.0.12"
+customization.config.version = "4.0.13-dev"
 customization.config.help_url = "https://github.com/pw4ever/awesome-wm-config/tree/" .. customization.config.version
 
 customization.default.property = {
