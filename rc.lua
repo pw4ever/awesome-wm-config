60846430a358e67d6aa73af7b59df7685989952c
Wei Peng <4pengw+git@gmail.com>
2019-12-11 17:37:24 -0800

    Broaden autostart scope to all visable to `dex`.

--- rc.lua
+++ rc.lua
@@ -2997,5 +2997,5 @@ end
 
 -- XDG style autostart with "dex"
 -- HACK continue
-awful.util.spawn_with_shell("if ! [ -e " .. awesome_autostart_once_fname .. " ]; then dex -a -e awesome; touch " .. awesome_autostart_once_fname .. "; fi")
+awful.util.spawn_with_shell("if ! [ -e " .. awesome_autostart_once_fname .. " ]; then dex -a; touch " .. awesome_autostart_once_fname .. "; fi")
 customization.func.client_opaque_on(nil) -- start xcompmgr
